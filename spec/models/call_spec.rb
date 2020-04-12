9028b9ad0205892edfe53aa6bf4774205d96fee3
John Bannister <jdbann@gmail.com>
2020-04-12 11:57:26 +0100

    Write proper Call specs
    - Adds shoulda-matchers gem

--- spec/models/call_spec.rb
+++ spec/models/call_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Call, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :round }
+
+  describe ".visile" do
+    let(:round) { Round.create(name: "Test Round") }
+
+    it "includes visible calls" do
+      call = round.calls.create(name: "Test Call", hidden: false)
+      expect(described_class.visible).to include call
+    end
+
+    it "excludes hidden calls" do
+      call = round.calls.create(name: "Test Call", hidden: true)
+      expect(described_class.visible).not_to include call
+    end
+  end
 end
