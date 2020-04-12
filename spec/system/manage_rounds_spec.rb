c6280a90f5230181a887c77dccb1134152a86cab
John Bannister <jdbann@gmail.com>
2020-04-12 11:51:08 +0100

    Ensure system tests run in headless chrome

--- spec/system/manage_rounds_spec.rb
+++ spec/system/manage_rounds_spec.rb
@@ -1,10 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "ManageRounds", type: :system do
-  before do
-    driven_by(:selenium_chrome)
-  end
-
   it "allows you to create rounds" do
     visit rounds_path
     click_on "New Round"
